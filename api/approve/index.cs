3b69860a5e6f01e1c792489bb2c03a07a11df5e5
ilwenz00 <lawrence.saliba@outlook.com>
2026-04-16 19:17:54 +0200

    Create index.cs

---
+++ api/approve/index.cs
@@ -0,0 +1,26 @@
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+public static class Approve
+{
+    [FunctionName("Approve")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "approve")] HttpRequest req)
+    {
+        string body = await new StreamReader(req.Body).ReadToEndAsync();
+        dynamic data = JsonConvert.DeserializeObject(body);
+        string id = data.id;
+
+        var reqItem = SubmitOvertime.Requests.FirstOrDefault(r => r.id == id);
+        if (reqItem == null) return new NotFoundObjectResult("Not found");
+
+        reqItem.status = "approved";
+        return new OkObjectResult("Approved");
+    }
+}
